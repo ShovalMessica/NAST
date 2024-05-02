d88994d78af35432f464e5a3b7016e186e3e898e
ShovalMessica <82871598+ShovalMessica@users.noreply.github.com>
2024-05-02 13:46:30 +0300

    Update cross_entropy_loss.py

--- losses/cross_entropy_loss.py
+++ losses/cross_entropy_loss.py
@@ -2,6 +2,7 @@ import torch
 
 class CrossEntropyLoss:
     def __call__(self, predicted, target):
+        # Augmentations alter the dimensions of the signal, linear interpolation is required to align and compare the two signals.
         traspose = torch.unsqueeze(predicted.T, dim=0)
         predicted_interpolated = torch.nn.functional.interpolate(traspose, target.shape[0])
         predicted_interpolated = torch.squeeze(predicted_interpolated, dim=0).T
