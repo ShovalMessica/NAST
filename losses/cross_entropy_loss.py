146132583e3dd4175b077157fc593174b5e87ea6
ShovalMessica <82871598+ShovalMessica@users.noreply.github.com>
2024-05-02 13:44:27 +0300

    Update cross_entropy_loss.py

--- losses/cross_entropy_loss.py
+++ losses/cross_entropy_loss.py
@@ -2,4 +2,8 @@ import torch
 
 class CrossEntropyLoss:
     def __call__(self, predicted, target):
+        traspose = torch.unsqueeze(predicted.T, dim=0)
+        predicted_interpolated = torch.nn.functional.interpolate(traspose, target.shape[0])
+        predicted_interpolated = torch.squeeze(predicted_interpolated, dim=0).T
+        
         return torch.nn.functional.cross_entropy(predicted, target)
