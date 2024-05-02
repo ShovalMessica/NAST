5fd54085f465c7c272f880932e442c78fd53753d
ShovalMessica <82871598+ShovalMessica@users.noreply.github.com>
2024-05-02 13:25:46 +0300

    Update diversity_loss.py

--- losses/diversity_loss.py
+++ losses/diversity_loss.py
@@ -1,1 +1,8 @@
+import torch
 
+class DiversityLoss:
+    def __call__(self, predictions, num_units):
+        mean_predictions = torch.sum(predictions, 0) / predictions.shape[0]
+        log_probs = torch.log(mean_predictions)
+        diversity_loss = torch.sum(mean_predictions * log_probs)
+        return diversity_loss / num_units
