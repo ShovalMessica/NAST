b9183f9793672e4d9ee9001b82a501fecdde9fae
shovalmessica <shovalmessica98@gmail.com>
2024-05-07 14:18:29 +0300

    fix bugs

--- main.py
+++ main.py
@@ -5,26 +5,23 @@ from models.network import Network
 from utils.checkpoint import load_checkpoint
 
 model_config_path = "/path/to/model_config.yaml"
-training_config_path = "/path/to/training_config.yaml"
 audio_path = "/path/to/audio.wav"
+num_units = 100
 
 with open(model_config_path, "r") as f:
     model_config = yaml.safe_load(f)
 
-with open(training_config_path, "r") as f:
-    training_config = yaml.safe_load(f)
-
-config = {**model_config, **training_config}
+config = {**model_config}
 
 config["discrete_local"] = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-feature_extractor = HubertFeatureReader(config['checkpoints']['hubert'], layer=9)
+feature_extractor = HubertFeatureReader(config['models']['hubert']['checkpoint_path'], layer=9)
 network = Network(config=config, device=device)
 
 # Load checkpoint if available
-if config['checkpoints']['ulm']:
-    load_checkpoint(network, config['checkpoints']['ulm'])
+if config['models'][num_units]["checkpoint_path"]:
+    load_checkpoint(network, config['models'][num_units]["checkpoint_path"])
 
 audio = feature_extractor.read_audio(audio_path)
 features = feature_extractor.get_feats(audio)
